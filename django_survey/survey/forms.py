b5893c71dfc9679095ca81268487bd3b8b19e191
Adam McQuistan <adam.mcquistan@licor.com>
2019-06-18 21:42:17 -0500

    part 4

--- django_survey/survey/forms.py
+++ django_survey/survey/forms.py
@@ -1,13 +1,13 @@
 # forms.py
 
-# from django import forms
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# class SurveyCreateForm(forms.Form):
-#     title = forms.CharField(label='Title', required=True, max_length=200)
-#     questions = forms.MultiValueField(
-#       required=True,
-#       label="Questions",
-#       widget=forms.MultipleHiddenInput()
-#     )
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=150)
 
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
--- django_survey/survey/urls.py
+++ django_survey/survey/urls.py
@@ -1,9 +1,11 @@
 # survey/urls.py
 
+from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
+from .tokens import user_tokenizer
 
 urlpatterns = [
   path('register/', views.RegisterView.as_view(), name='register'),
@@ -13,5 +15,25 @@ urlpatterns = [
   path('surveys/create/', views.SurveyCreateView.as_view(), name='survey_create'),
   path('survey-assginment/<int:assignment_id>/', views.SurveyAssignmentView.as_view(), name='survey_assignment'),
   path('survey-management/<int:survey_id>/', views.SurveyManagerView.as_view(), name='survey_management'),
-  path('survey-results/<int:survey_id>/', views.SurveyResultsView.as_view(), name='survey_results')
+  path('survey-results/<int:survey_id>/', views.SurveyResultsView.as_view(), name='survey_results'),
+  path('confirm-email/<str:user_id>/<str:token>/', views.ConfirmRegistrationView.as_view(), name='confirm_email'),
+  path(
+    'reset-password/',
+    auth_views.PasswordResetView.as_view(
+      template_name='survey/reset_password.html',
+      html_email_template_name='survey/reset_password_email.html',
+      success_url=settings.LOGIN_URL,
+      token_generator=user_tokenizer),
+    name='reset_password'
+  ),
+  path(
+    'reset-password-confirmation/<str:uidb64>/<str:token>/',
+    auth_views.PasswordResetConfirmView.as_view(
+      template_name='survey/reset_password_update.html', 
+      post_reset_login=True,
+      post_reset_login_backend='django.contrib.auth.backends.ModelBackend',
+      token_generator=user_tokenizer,
+      success_url=settings.LOGIN_REDIRECT_URL),
+    name='password_reset_confirm'
+  ),
 ]
